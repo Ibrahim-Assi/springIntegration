7c32d125cf73d4a1cb43fd44a9c5e186a83c3c14
ibrahimassi <ibrahimassi@fe80:0:0:0:aede:48ff:fe00:1122%8>
2019-04-13 20:31:36 +0300

    begin read Rss through direct channel feed

--- spring-integration/src/main/java/ims/health/Appliaction.java
+++ spring-integration/src/main/java/ims/health/Appliaction.java
@@ -1,7 +1,14 @@
 package ims.health;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHandler;
+import org.springframework.messaging.MessagingException;
+
+import com.rometools.rome.feed.synd.SyndEntry;
 
 import ims.health.configrations.AppConfig;
 
@@ -13,15 +20,30 @@ public class Appliaction
 {
 	
 	
-    public static void main( String[] args )
+    public static void main( String[] args ) throws InterruptedException
     {
     	@SuppressWarnings("resource")
-		ApplicationContext context = new ClassPathXmlApplicationContext("integration.xml");
+    	AbstractApplicationContext context = new ClassPathXmlApplicationContext("integration-config.xml");
 		AppConfig appConfig = (AppConfig) context.getBean("appConf");
-		
-		
 		
 		System.out.println("Rss link:"+ appConfig.getRssLink());
 		System.out.println("rootPath:"+appConfig.getRootFolderPath());
+		
+		
+		// direct channel feed
+		
+		 try {            
+	            DirectChannel channel = context.getBean("aljazeeraChannel", DirectChannel.class);
+	            channel.subscribe(new MessageHandler() {
+	                
+	                public void handleMessage(Message<?> message) throws MessagingException {
+	                    SyndEntry entry = (SyndEntry) message.getPayload();
+	                    System.out.println(entry.getPublishedDate() + " - " + entry.getTitle());
+	                }
+	            });
+	            Thread.sleep(5000);
+	        } finally {
+	            context.close();
+	        }
     }
 }
